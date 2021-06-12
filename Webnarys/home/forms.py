f5fc81520800daf6470bbfaeee016d52d0b6f1c8
KoyalThakur <koyalthakur15@gmail.com>
2021-06-12 23:24:45 +0530

    Listings page in progress

--- Webnarys/home/forms.py
+++ Webnarys/home/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+from django.forms import modelformset_factory
+from .models import Property
 
 
 class SignUpForm(UserCreationForm):
@@ -10,3 +12,5 @@ class SignUpForm(UserCreationForm):
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2', )
+
+PropertyFormset= modelformset_factory(Property, fields=("institute_name", "address", "rent"), extra=1)
--- Webnarys/home/urls.py
+++ Webnarys/home/urls.py
@@ -2,13 +2,15 @@ from django.contrib import admin
 from django.urls import path,include
 from home import views
 from django.conf.urls import url
+from .views import PropertyAddView, PropertyListView
 
 urlpatterns = [
     path('',views.index,name='home'),
     path('about',views.about,name='about'),
     path('contact',views.contact,name='contact'),
     path('signIn',views.signin,name='signin'),
-    # path('signUp', views.signUphandle, name='signUphandle'),
     path('searchResult',views.searchResult,name='searchResult'),
-   url(r'^signUp/$', views.signup, name='signup'),
+    url(r'^signUp/$', views.signup, name='signup'),
+    path('listings', PropertyListView.as_view(), name='listings'),
+    path('add', PropertyAddView.as_view(), name='add')
 ]
