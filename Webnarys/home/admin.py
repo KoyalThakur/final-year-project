49ec812d480ec7f3300e64906a38cec625f57fe4
KoyalThakur <koyalthakur15@gmail.com>
2021-06-13 22:40:14 +0530

    Listings done

--- Webnarys/home/admin.py
+++ Webnarys/home/admin.py
@@ -1,4 +1,6 @@
+from .models import Property
 from django.contrib import admin
 from home.models import Contact
 admin.site.register(Contact)
+admin.site.register(Property)
 # Register your models here.
